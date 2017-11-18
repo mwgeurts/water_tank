70f04507fae710a1af48b4f542bbb4b8f05d7594
Mark Geurts <mark.w.geurts@gmail.com>
2017-11-17 19:50:21 -0500

    Added manual, FWXM center options

--- private/CenterProfiles.m
+++ private/CenterProfiles.m
@@ -21,11 +21,16 @@ function profiles = CenterProfiles(varargin)
 % You should have received a copy of the GNU General Public License along 
 % with this program. If not, see http://www.gnu.org/licenses/.
 
+% Persistently store fractional value
+persistent frac shift;
+
 % Specify options and order
 options = {
     'None'
+    'Manual'
     'FWHM'
     'FWQM'
+    'FWXM'
 };
 
 % If no input arguments are provided
@@ -46,9 +51,45 @@ switch varargin{2}
         
         % Return raw profile
         profiles = varargin{1};
+    
+    % Manual
+    case 2
+        
+        % Start raw profile
+        profiles = varargin{1};
+        
+        % If shift is not defined
+        if ~exist('shift', 'var') || isempty(shift) || isnan(shift(1)) || ...
+                isnan(shift(2))
+            shift = [0 0];
+        end
+        
+        % Ask user for shift
+        shift = str2double(inputdlg({'Enter IEC X shift (mm):', ...
+            'Enter IEC Y shift (mm):'}, 'Manual Center', 1, ...
+            {sprintf('%0.1f', shift(1)), sprintf('%0.1f', shift(2))}));
+        
+        % If the user provided a valid value
+        if ~isempty(shift) && ~isnan(shift(1)) && ~isnan(shift(2)) && ...
+                (nargin < 3 || varargin{3} == 0)
+            
+            % Log action 
+            Event(sprintf(['Shifting profiles by %0.1f mm in IEC-X and ', ...
+                '%0.1f mm in IEC-Y'], shift(1), shift(2)));
+
+            % Loop through each profile
+            for i = 1:length(profiles)
+
+                % Shift IEC X
+                profiles{i}(:,1) = profiles{i}(:,1) + shift(1);
+                
+                % Shift IEC Y
+                profiles{i}(:,2) = profiles{i}(:,2) + shift(2);
+            end
+        end
         
     % FWHM
-    case 2
+    case 3
         
         % Start with raw profile
         profiles = varargin{1};
@@ -98,7 +139,7 @@ switch varargin{2}
         clear i;
         
     % FWQM
-    case 3
+    case 4
         
         % Start with raw profile
         profiles = varargin{1};
@@ -140,6 +181,84 @@ switch varargin{2}
                     % Re-interpolate reference with to center it
                     [~, ~, profiles{i}(:,5)] = FWXM(profiles{i}(:,2), ...
                         profiles{i}(:,5), 0.25);
+                end
+            end
+        end 
+        
+        % Clear temporary variables
+        clear i;
+        
+    % FWXM
+    case 5
+        
+        % Start with raw profile
+        profiles = varargin{1};
+        
+        % If shift is not defined
+        if ~exist('frac', 'var') || isempty(frac) || isnan(frac)
+            frac = 0.5;
+        end
+        
+        % Prompt user to enter fractional value or percentage
+        frac = inputdlg(['Enter fractional value or percentage to center ', ...
+            'profiles on:'], 'Enter Fraction', 1, {sprintf('%0.2f', frac)});
+        
+        % If the user clicked cancel, do not shift
+        if isempty(frac)
+            return;
+        
+        % Convert to number
+        elseif iscell(frac)
+            frac = str2double(strrep(frac{1}, '%', ''));
+        end
+        
+        % If fractional value is NaN
+        if isnan(frac)
+            return;
+        
+        % If value is greater than 1, assume user provided percentage
+        elseif frac > 1
+            frac = frac / 100;
+        end
+        
+        % Log action 
+        Event(sprintf('Centering profiles by %0.2f maximum', frac));
+        if nargin >= 3 && varargin{3} == 1
+            Event(sprintf('Centering reference profiles by %0.2f maximum', ...
+                frac));
+        end
+
+        % Loop through each profile
+        for i = 1:length(profiles)
+        
+            % If X changes, this is an X profile
+            if profiles{i}(1,1) ~= profiles{i}(2,1)
+                
+                % Shift measured profile by offset
+                profiles{i}(:,1) = profiles{i}(:,1) - ...
+                    FWXM(profiles{i}(:,1), profiles{i}(:,4), frac);
+                
+                % If center reference flag is set
+                if size(profiles{i},2) >= 5 && nargin >= 3 && varargin{3} == 1
+                    
+                    % Re-interpolate reference with to center it
+                    [~, ~, profiles{i}(:,5)] = FWXM(profiles{i}(:,1), ...
+                        profiles{i}(:,5), frac);
+                end
+
+            % Otherwise, if Y changes, this is an Y profile
+            elseif profiles{i}(1,2) ~= profiles{i}(2,2)
+                
+                % Shift measured profile by offset
+                profiles{i}(:,2) = profiles{i}(:,2) - ...
+                    FWXM(profiles{i}(:,2), profiles{i}(:,4), frac);
+                
+                % If center reference flag is set
+                if size(profiles{i},2) >= 5 && nargin >= 3 && varargin{3} == 1
+                    
+                    % Re-interpolate reference with to center it
+                    [~, ~, profiles{i}(:,5)] = FWXM(profiles{i}(:,2), ...
+                        profiles{i}(:,5), frac);
                 end
             end
         end 
